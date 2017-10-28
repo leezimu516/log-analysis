f3829ddd4d58f623c8a8ffed07d5e2020882d363
kai li <adamlee516@gmail.com>
2017-10-28 11:14:01 -0400

    modify query3 to improve effciency change ouput format

--- analysis-db.py
+++ analysis-db.py
@@ -1,11 +1,12 @@
+#!/usr/bin/env python
 import psycopg2
 
-question1 = ''' 1. What are the most popular three articles of all time? '''
+question1 = '''1. What are the most popular three articles of all time? '''
 query1 = ('''
     select articles.title, count(*) as views
     from articles
     join log
-    on log.path like concat('%', articles.slug, '%')
+    on log.path = concat('/article/', articles.slug)
     where log.status like '%200%'
     group by articles.title, log.path
     order by views desc
@@ -19,17 +20,16 @@ query2 = ('''
     join articles
     on authors.id = articles.author
     join log
-    on log.path like concat('%', articles.slug, '%')
+    on log.path = concat('/article/', articles.slug)
     where log.status like '%200%'
     group by authors.name
-    order by views desc
-    limit 3;
+    order by views desc;
     ''')
 
-question3 = ''' On which days did more than 1% of requests lead to errors?'''
+question3 = '''3.On which days did more than 1% of requests lead to errors?'''
 query3 = ("""
-    SELECT date, rate
-    FROM (SELECT to_char(log.time, 'Mon DD, YYYY') as date,
+    SELECT to_char(date, 'Mon DD, YYYY') as date, rate
+    FROM (SELECT date(log.time) as date,
           round(cast(100 * sum(CASE WHEN log.status like '%404%'
           THEN 1 ELSE 0 END)::float /
           count(*) AS numeric), 2) as rate
@@ -45,16 +45,32 @@ def get_results(query):
     pg = psycopg2.connect("dbname=news")
     c = pg.cursor()
     c.execute(query)
-    results =  c.fetchall()
-    for item in results:
-        print item
+    results = c.fetchall()
     pg.close()
+    return results
+
+
+def print_result(query_results):
+    # print question
+    print query_results[0]
+    for result in query_results[1]:
+        # output format: article -- 1000 views
+        print '  ' + result[0] + ' -- ' + str(result[1]) + ' views'
+    print '\n'
+
+
+def print_result_err(query_results):
+    # print question
+    print query_results[0]
+    for result in query_results[1]:
+        # output format: article -- 1000 views
+        print '  ' + result[0] + ' -- ' + str(result[1]) + ' %'
 
 if __name__ == '__main__':
-    # make question list associate with according query
-    querys = [query1, query2, query3]
-    questions = [question1, question2, question3]
-    for question, query in zip(questions, querys):
-        print question
-        get_results(query)
-        print '\n'
+    article_results = question1, get_results(query1)
+    author_results = question2, get_results(query2)
+    error_results = question3, get_results(query3)
+
+    print_result(article_results)
+    print_result(author_results)
+    print_result_err(error_results)
